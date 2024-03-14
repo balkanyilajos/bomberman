3e11eafd8c1c137a22778c9a3d4c4295836e6bcd
Balkányi Lajos <balkanyilajos@gmail.com>
2024-03-14 12:54:25 +0100

    implement Action and PlayerAction classes. Now, it is possible to set and get directions and other actions

--- src/model/util/Action.java
+++ src/model/util/Action.java
@@ -1,5 +1,47 @@
 package model.util;
 
 public class Action {
+    private boolean left;
+    private boolean right;
+    private boolean up;
+    private boolean down;
     
+    public Action() {
+        left = right = up = down = false;
+    }
+    
+    public boolean isLeft() {
+        return left;
+    }
+
+    public void setLeft(boolean left) {
+        this.left = left;
+    }
+
+    public boolean isRight() {
+        return right;
+    }
+
+    public void setRight(boolean right) {
+        this.right = right;
+    }
+
+    public boolean isUp() {
+        return up;
+    }
+
+    public void setUp(boolean up) {
+        this.up = up;
+    }
+
+    public boolean isDown() {
+        return down;
+    }
+
+    public void setDown(boolean down) {
+        this.down = down;
+    }
+
+    
+
 }
--- src/model/util/PlayerAction.java
+++ src/model/util/PlayerAction.java
@@ -1,5 +1,30 @@
 package model.util;
 
-public class PlayerAction {
+public class PlayerAction extends Action {
     
+    private boolean placeBomb;
+    private boolean placeBarrier;
+
+    public PlayerAction() {
+        super();
+        placeBomb = placeBarrier = false;
+    }
+
+
+    public boolean isPlaceBomb() {
+        return placeBomb;
+    }
+
+    public void setPlaceBomb(boolean placeBomb) {
+        this.placeBomb = placeBomb;
+    }
+
+    public boolean isPlaceBarrier() {
+        return placeBarrier;
+    }
+
+    public void setPlaceBarrier(boolean placeBarrier) {
+        this.placeBarrier = placeBarrier;
+    }    
+
 }
