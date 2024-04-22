b18b4959a98ed2261229b0ca908e8f949e215aa0
Balkányi Lajos <balkanyilajos@gmail.com>
2024-04-22 15:42:05 +0200

    add more simple bomb tests

--- test/BombTest.java
+++ test/BombTest.java
@@ -13,6 +13,8 @@ import model.MapReader;
 import model.sprite.Sprite;
 import model.sprite.fixedelement.Barrier;
 import model.sprite.fixedelement.Box;
+import model.sprite.fixedelement.Wall;
+import model.sprite.moveable.player.Player;
 import model.sprite.weapon.Bomb;
 
 public class BombTest {
@@ -69,4 +71,63 @@ public class BombTest {
         Assert.assertEquals(afterBomb.size(), original.size()-2);
         model.gameStop(true);
     }
+
+    @Test
+    public void bombWall() {
+        model.init(map, 0, 1);
+        model.start((Dimension)cubeSize.clone());
+        model.addSpriteToBoard(new Wall(model, new Point2D.Double(0*cubeSize.width, 0*cubeSize.height)));
+        model.addSpriteToBoard(new Wall(model, new Point2D.Double(1*cubeSize.width, 0*cubeSize.height)));
+        model.addSpriteToBoard(new Bomb(model, new Point2D.Double(2*cubeSize.width, 0*cubeSize.height), 2.0 * cubeSize.width, 0));
+
+        HashSet<Sprite> original = model.getBoardSprites();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        HashSet<Sprite> afterBomb = model.getBoardSprites();
+        Assert.assertEquals(afterBomb.size(), original.size()-1);
+        model.gameStop(true);
+    }
+
+    @Test
+    public void bombBomb() {
+        model.init(map, 0, 1);
+        model.start((Dimension)cubeSize.clone());
+        model.addSpriteToBoard(new Bomb(model, new Point2D.Double(0*cubeSize.width, 0*cubeSize.height), 2.0 * cubeSize.width));
+        model.addSpriteToBoard(new Bomb(model, new Point2D.Double(1*cubeSize.width, 0*cubeSize.height), 2.0 * cubeSize.width));
+        model.addSpriteToBoard(new Bomb(model, new Point2D.Double(2*cubeSize.width, 0*cubeSize.height), 2.0 * cubeSize.width, 0));
+
+        HashSet<Sprite> original = model.getBoardSprites();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        HashSet<Sprite> afterBomb = model.getBoardSprites();
+        Assert.assertEquals(afterBomb.size(), original.size()-3);
+        model.gameStop(true);
+    }
+
+    @Test
+    public void bombPlayer() {
+        model.init(map, 1, 1);
+        model.start((Dimension)cubeSize.clone());
+        model.addSpriteToBoard(new Player(model, new Point2D.Double(0*cubeSize.width, 0*cubeSize.height), "src/data/picture/player/player.png"));
+        model.addSpriteToBoard(new Bomb(model, new Point2D.Double(1*cubeSize.width, 0*cubeSize.height), 2.0 * cubeSize.width, 0));
+        
+        HashSet<Sprite> original = model.getBoardSprites();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        HashSet<Sprite> afterBomb = model.getBoardSprites();
+        Assert.assertEquals(afterBomb.size(), original.size()-2);
+        model.gameStop(true);
+    }
+
+    
+
 }
