c2654bfb8d984c8f5470c1af724eba8d2d5b7e63
bbjpni <bbjpni@inf.elte.hu>
2024-04-23 22:18:07 +0200

    BoomBooster without effect implementation and havs bug when it add to board

--- src/model/sprite/powerup/BombBooster.java
+++ src/model/sprite/powerup/BombBooster.java
@@ -1,5 +1,31 @@
 package model.sprite.powerup;
 
-public class BombBooster {
-    
+import gui.game.BoardPanel;
+import model.GameModel;
+import model.sprite.Sprite;
+import model.sprite.moveable.player.Player;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.geom.Area;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.io.File;
+import java.io.IOException;
+
+public class BombBooster extends PowerUp {
+
+    public BombBooster(GameModel model, Point2D point) {
+        this(model, point, model.getCubeSize(), new Point2D.Double(point.getX() - model.getCubeSize().width * 2 * 0.3, point.getY() - model.getCubeSize().height * 1.4 * 0.18), new Dimension((int) (model.getCubeSize().width * 2), (int) (model.getCubeSize().height * 1.4)));
+    }
+
+    public BombBooster(GameModel model, Point2D areaPoint, Dimension areaSize, Point2D imagePoint, Dimension imageSize) {
+        super(model, new Area(new Rectangle2D.Double(areaPoint.getX(), areaPoint.getY(), areaSize.width, areaSize.height)),
+            imagePoint, areaPoint, imageSize, "src/data/picture/powerup/shield.png");
+    }
+
+    @Override
+    public void effect(Player player)
+    {}
 }
--- src/model/sprite/powerup/PowerUp.java
+++ src/model/sprite/powerup/PowerUp.java
@@ -1,5 +1,28 @@
 package model.sprite.powerup;
 
-public class PowerUp {
-    
+import gui.game.BoardPanel;
+import model.GameModel;
+import model.sprite.Sprite;
+import model.sprite.moveable.player.Player;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.geom.Area;
+import java.awt.geom.Point2D;
+import java.io.File;
+import java.io.IOException;
+import java.awt.geom.Rectangle2D;
+
+public abstract class PowerUp extends Sprite {
+
+    public PowerUp(GameModel model, Area area, Point2D imagePoint,
+            Point2D areaPoint,
+            Dimension imageSize,
+            String imagePath) 
+    {
+        super(model, area, imagePoint, areaPoint, imageSize, imagePath);
+    }
+
+    public abstract void effect(Player player);
 }
