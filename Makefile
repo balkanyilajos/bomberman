13d9ffbc2ad504c858a68a556d00ae9b3e948d83
Balkányi Lajos <balkanyilajos@gmail.com>
2024-04-28 22:29:01 +0200

    Make more tests and refresh makefile

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
 run: ./src/main/Main.java
 	@javac -cp ".\src" .\src\main\Main.java -d bin
 	@java -cp ".\bin" main.Main
+
+test: ./test/*.java ./lib/junit-*.jar
+	@javac -d bin -cp ".\lib\junit-platform-console-standalone-1.10.2.jar;.\src" test/BombTest.java
+	@java -jar ".\lib\junit-platform-console-standalone-1.10.2.jar" --class-path ".\bin" --scan-class-path
+
--- test/BombTest.java
+++ test/BombTest.java
@@ -10,29 +10,22 @@ import org.junit.*;
 
 import model.GameModel;
 import model.MapReader;
-import model.sprite.Sprite;
-import model.sprite.fixedelement.Barrier;
-import model.sprite.fixedelement.Box;
-import model.sprite.fixedelement.Wall;
+import model.sprite.*;
+import model.sprite.fixedelement.*;
+import model.sprite.moveable.enemy.Balloon;
 import model.sprite.moveable.player.Player;
+import model.sprite.powerup.*;
 import model.sprite.weapon.Bomb;
 
 public class BombTest {
     private final GameModel model;
     private final Dimension cubeSize;
-    private Dimension boardSize;
     private String map;
 
     public BombTest() {
         map = "test/standard-map.txt";
         model = new GameModel(null, map, 1, new ArrayList<>());
         cubeSize = new Dimension(50, 50);
-        try {
-            boardSize = MapReader.getBoardIndexSize(map);
-        }
-        catch(IOException e) {
-            System.out.println(e);
-        }
     }
 
     @Test
@@ -49,6 +42,8 @@ public class BombTest {
             e.printStackTrace();
         }
         HashSet<Sprite> afterBomb = model.getBoardSprites();
+        afterBomb.removeIf(sprite -> sprite instanceof PowerUp);
+
         Assert.assertEquals(afterBomb.size(), original.size()-2);
         model.gameStop(true);
     }
@@ -115,7 +110,7 @@ public class BombTest {
         model.init(map, 1, 1);
         model.start((Dimension)cubeSize.clone());
         model.addSpriteToBoard(new Player(model, new Point2D.Double(0*cubeSize.width, 0*cubeSize.height), "src/data/picture/player/player.png"));
-        model.addSpriteToBoard(new Bomb(model, new Point2D.Double(1*cubeSize.width, 0*cubeSize.height), 2.0 * cubeSize.width, 0));
+        model.addSpriteToBoard(new Bomb(model, new Point2D.Double(2*cubeSize.width, 0*cubeSize.height), 2.0 * cubeSize.width, 0));
         
         HashSet<Sprite> original = model.getBoardSprites();
         try {
@@ -128,6 +123,40 @@ public class BombTest {
         model.gameStop(true);
     }
 
-    
+    @Test
+    public void bombBombBooster() {
+        model.init(map, 1, 1);
+        model.start((Dimension)cubeSize.clone());
+        model.addSpriteToBoard(new BombBooster(model, new Point2D.Double(0*cubeSize.width, 0*cubeSize.height)));
+        model.addSpriteToBoard(new Bomb(model, new Point2D.Double(2*cubeSize.width, 0*cubeSize.height), 2.0 * cubeSize.width, 0));
+        
+        HashSet<Sprite> original = model.getBoardSprites();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        HashSet<Sprite> afterBomb = model.getBoardSprites();
+        Assert.assertEquals(afterBomb.size(), original.size()-1);
+        model.gameStop(true);
+    }
+
+    @Test
+    public void bombBalloon() {
+        model.init(map, 1, 1);
+        model.start((Dimension)cubeSize.clone());
+        model.addSpriteToBoard(new Balloon(model, new Point2D.Double(0*cubeSize.width, 0*cubeSize.height), "src/data/picture/monster/dino1.png"));
+        model.addSpriteToBoard(new Bomb(model, new Point2D.Double(2*cubeSize.width, 0*cubeSize.height), 2.0 * cubeSize.width, 0));
+        
+        HashSet<Sprite> original = model.getBoardSprites();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        HashSet<Sprite> afterBomb = model.getBoardSprites();
+        Assert.assertEquals(afterBomb.size(), original.size()-2);
+        model.gameStop(true);
+    }
 
 }
